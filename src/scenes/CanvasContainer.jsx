1ce930245b4785b3cc17abc1e4ef5edcdc33ab03
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2024-07-11 23:58:47 -0300

    fluid movement in cube

--- src/scenes/CanvasContainer.jsx
+++ src/scenes/CanvasContainer.jsx
@@ -1,5 +1,6 @@
-import { Environment , OrbitControls} from '@react-three/drei';
+import { Cloud, Clouds, Environment , Float, MeshWobbleMaterial, OrbitControls} from '@react-three/drei';
 import { Canvas } from '@react-three/fiber'
+
 
 
 export const CanvasContainer = () => {
@@ -10,16 +11,25 @@ export const CanvasContainer = () => {
         shadows
         camera={{ position: [0, 0, 3], fov : 45 }}
         >
-
           <OrbitControls autoRotate autoRotateSpeed={4}/>
-          <Environment preset='park' />
-
+          <Environment preset='park' blur={5} />
           <ambientLight />
-          <mesh>
-              <boxGeometry />
-              <meshStandardMaterial envMapIntensity={1} roughness={0.1} metalness={.9} />
-
-          </mesh>
+          <Clouds>
+            <Cloud concentrate="outside" seed={1} segments={100} bounds={20} volume={20} growth={10} opacity={0.15} position={[0, 0, -10]} speed={1} />
+          </Clouds>
+          <Float floatIntensity={1} rotationIntensity={10} speed={1}>
+            <mesh >
+                <boxGeometry />
+                
+                <MeshWobbleMaterial
+                  factor={2}
+                  speed={1.5}
+                  roughness={0.2}
+                  metalness={0.9}
+                  envMapIntensity={1}
+                  />
+            </mesh>
+          </Float>
         </Canvas>
     </main>
   )
