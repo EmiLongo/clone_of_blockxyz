9d45c09a74b40293f84fe7248d94e76b51e3595e
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2024-07-12 18:35:02 -0300

    gradient background is added

--- src/scenes/CanvasContainer.jsx
+++ src/scenes/CanvasContainer.jsx
@@ -3,7 +3,8 @@ import { Canvas } from '@react-three/fiber'
 import useIsMobile from '@hooks/useIsMobile';
 export const CanvasContainer = ({isAnimating, isGrayscale}) => {
   const isMobile = useIsMobile()
-
+  const a = <Environment>...</Environment>
+  console.log(a)
   return (
     <main id="canvas-container"
           style={{
@@ -12,15 +13,20 @@ export const CanvasContainer = ({isAnimating, isGrayscale}) => {
           }}
     >
         <Canvas 
-        className='canvas'
-        shadows
-        camera={{ position: [0, 0, 3], fov : 45 }}
+          className='canvas'
+          shadows
+          camera={{ position: [0, 0, 3], fov : 45 }}
         >
-          <OrbitControls autoRotate={isAnimating} autoRotateSpeed={4}/>
-          <Environment preset='lobby' blur={1} />
+          <OrbitControls 
+            autoRotate={isAnimating}
+            autoRotateSpeed={4}
+          />
+          <Environment 
+            preset='park'
+          />
           <ambientLight />
           <Clouds>
-            <Cloud concentrate="outside" seed={1} segments={100} bounds={20} volume={20} growth={1} opacity={0.15} position={[0, 0, -10]} speed={isAnimating ? 1 : 0} />
+            <Cloud concentrate="outside" seed={1} segments={100} bounds={20} volume={20} growth={1} opacity={0.10} position={[0, 0, -10]} speed={isAnimating ? 1 : 0} />
           </Clouds>
           <Float floatIntensity={1} rotationIntensity={10} speed={isAnimating ? 1 : 0}>
             <mesh scale={isMobile ? [0.6, 0.6, 0.6] : [1, 1, 1]}>
@@ -34,6 +40,18 @@ export const CanvasContainer = ({isAnimating, isGrayscale}) => {
                   />
             </mesh>
           </Float>
+          {/* <mesh position={[-10 , 1, 1]}>
+            <planeGeometry args={[100, 100, 1, 1]} />
+            <meshBasicMaterial
+              noise={true}
+              noiseMap={'https://images.unsplash.com/photo-1579546929662-711aa81148cf'}
+              // map-flipY={false}
+              // roughness={1}
+              // roughnessMap-flipY={false}
+              // map-anisotropy={16}
+
+            />
+          </mesh> */}
         </Canvas>
     </main>
   )
